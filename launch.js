8ab4517ac35ded962f9c665c97eb533e3b82df0f
Joe Rattazzi <joerattazzi@gmail.com>
2018-12-21 12:13:12 -0600

    try again...

--- launch.js
+++ launch.js
@@ -3,7 +3,11 @@ import 'fhirclient';
 console.log("authorizing");
 console.log("authorizing again!");
 
-Window.FHIR.oauth2.authorize({
-    'client_id': 'b65d7ea8-73ef-4892-ad22-21a21c0b4c1b',
-    'scope':  'patient/Patient.read patient/Observation.read launch online_access openid profile'
-});
+(function(window){
+    window.extractData = function() {
+        Window.FHIR.oauth2.authorize({
+            'client_id': 'b65d7ea8-73ef-4892-ad22-21a21c0b4c1b',
+            'scope': 'patient/Patient.read patient/Observation.read launch online_access openid profile'
+        });
+    }
+})(window);
