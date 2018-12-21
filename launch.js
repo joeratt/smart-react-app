828dfe059ec139d81f8e9d8eb335b9ec24c0ba6b
Joe Rattazzi <joerattazzi@gmail.com>
2018-12-21 11:53:15 -0600

    Fix launch.js

--- launch.js
+++ launch.js
@@ -2,7 +2,7 @@ import 'fhirclient';
 
 console.log("authorizing");
 
-FHIR.oauth2.authorize({
+Window.FHIR.oauth2.authorize({
     'client_id': 'b65d7ea8-73ef-4892-ad22-21a21c0b4c1b',
     'scope':  'patient/Patient.read patient/Observation.read launch online_access openid profile'
 });
